e973c3fccc99663c084cfb2aac4d1641c60fb8c4
Rod Angelo <vendiolaangelo2@gmail.com>
2025-10-06 17:50:02 +0800

    Update files and made some fixes

--- src/components/products/Productcard.jsx
+++ src/components/products/Productcard.jsx
@@ -5,7 +5,7 @@ function ProductCard({ item }) {
       <p className="font-semibold">{item.name}</p>
       <p className="text-sm text-gray-500">{item.category}</p>
       <p className="text-sm text-gray-500">${item.price.toFixed(2)}</p>
-      <p className="text-sm text-yellow-500">{item.rating}</p>
+      <p className="text-sm text-yellow-500">{item.rating} Star</p>
     </div>
   );
 }
